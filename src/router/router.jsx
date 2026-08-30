294ce14611c780fb41f5298851e8c45e14b2a3ed
chandru4527 <chandrukumar4527@gmail.com>
2026-08-30 20:55:57 +0530

    feat: add routing for admin layout with Button and Card elements

---
+++ src/router/router.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import AdminLayout from "../layout/AdminLayout";
+import Dashboard from "../Pages/Dashboard";
+import ButtonElement from '../Pages/ButtonElement'
+import CardElement from '../Pages/CardElement'
+
+const routers = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/admin" replace />,
+  },
+  {
+    path: "/admin",
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+      {
+        path:'buttons',
+        element:<ButtonElement/>
+      },
+      {
+        path:'cards',
+        element:<CardElement/>
+      }
+    ],
+  },
+]);
+
+export default routers;
