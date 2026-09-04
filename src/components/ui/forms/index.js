73d07c6fd0851ba81a7e09febc3ea7932781c8d6
chandru4527 <chandrukumar4527@gmail.com>
2026-09-04 16:32:04 +0530

    feat: remove unused components and clean up configuration

--- src/components/ui/forms/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Input } from "./Input";
-export { default as Textarea } from "./Textarea";
-export { default as Select } from "./Select";
-export { default as Checkbox } from "./Checkbox";
-export { default as Radio } from "./Radio";
-export { default as Switch } from "./Switch";
-export { default as FileInput } from "./FileInput";
-export { default as DateFilter } from "./DateFilter";
